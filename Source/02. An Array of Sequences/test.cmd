46db56d49d4ba5a3f6c291e3c6092fccd359cf8b
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-06-04 16:29:36 +0300

    Построение декартова произведения с помощью спискового включения.

--- Source/02. An Array of Sequences/test.cmd
+++ Source/02. An Array of Sequences/test.cmd
@@ -1,3 +1,4 @@
 python -m doctest symbol_codes.doctest
 python -m doctest listcomp_genexp_local_scope.doctest
 python -m doctest listcomp_vs_filter_and_map.doctest
+python -m doctest cartesian_product.doctest
