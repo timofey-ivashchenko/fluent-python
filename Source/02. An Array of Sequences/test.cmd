41e4cd91f111446373aca28d485a347be1889d16
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-06-10 12:00:03 +0300

    Переработан пример 2.4.

--- Source/02. An Array of Sequences/test.cmd
+++ Source/02. An Array of Sequences/test.cmd
@@ -1,3 +1,2 @@
-python -m doctest cartesian_product.doctest
 python -m doctest initializing_a_tuple_and_an_array_from_a_generator_expression.doctest
 python -m doctest cartesian_product_in_a_generator_expression.doctest
