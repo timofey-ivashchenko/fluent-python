74fb64c60638eeec6a21717127fbbf8389f23706
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-06-26 19:59:36 +0300

    Комментарии в классе Robot.

--- 02. An Array of Sequences/Examples/Robot.py
+++ 02. An Array of Sequences/Examples/Robot.py
@@ -26,15 +26,30 @@ class Robot:
         print(f'Robot.beep(frequency={frequency}, times={times})')
 
     def handle_command(self, message):
+        # The expression after the match keyword is the subject. The subject is
+        # the data that Python will try to match to the patterns in each case
+        # clause.
         match message:
+            # This pattern matches any subject that is a sequence with three
+            # items. The first item must be the string 'BEEPER'. The second and
+            # third item can be anything, and they will be bound to the
+            # variables frequency and times, in that order.
             case ['BEEPER', frequency, times]:
                 Robot.beep(frequency, times)
+            # This matches any subject with two items, the first being 'NECK'.
             case ['NECK', angle]:
                 Robot.rotate_neck(angle)
+            # This will match a subject with three items starting with 'LED'.
+            # If the number of items does not match, Python proceeds to the
+            # next case.
             case ['LED', id_, intensity]:
                 self.leds[id_].set_brightness(intensity)
+            # Another sequence pattern starting with 'LED', now with five
+            # items–including the 'LED' constant.
             case ['LED', id_, red, green, blue]:
                 self.leds[id_].set_color(red, green, blue)
+            # This is the default case. It will match any subject that did not
+            # match a previous pattern. The _ variable is special.
             case _:
                 raise InvalidCommand(message)
 
